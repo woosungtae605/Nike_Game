c5fd0e3cb7b86303293252b71986d5b332a9dab2
woosungtae <a01076755795@gmail.com>
2026-03-09 23:12:11 +0900

    add : art

--- Assets/Code/Player/PlayerMove.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Code.Player
-{
-    public class PlayerMove : MonoBehaviour
-    {
-        
-    }
-}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    [CreateAssetMenu(fileName = "inputSystem", menuName = "SO/inputSystem", order = 0)]
+    public class PlayerInput : ScriptableObject, Controller.IPlayerActions
+    {
+        public event Action<Vector2> OnMovementPressed;
+        
+        private Controller _inputSo;
+        private void OnEnable()
+        {
+            if (_inputSo == null)
+            {
+                _inputSo = new Controller();
+                _inputSo.Player.SetCallbacks(this);
+            }
+            
+            _inputSo.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputSo.Player.Disable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            Vector2 moveDir = context.ReadValue<Vector2>();
+            OnMovementPressed?.Invoke(moveDir);
+        }
+    }
+}
