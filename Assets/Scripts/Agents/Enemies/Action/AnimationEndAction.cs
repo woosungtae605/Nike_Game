4224ee3d9829a0a1bb0b5938505f08f225a50690
woosungtae <a01076755795@gmail.com>
2026-06-13 04:14:35 +0900

    feat : camo AnimationSetting

---
+++ Assets/Scripts/Agents/Enemies/Action/AnimationEndAction.cs
@@ -0,0 +1,45 @@
+using System;
+using Agents.Module;
+using Unity.Behavior;
+using Unity.Properties;
+using UnityEngine;
+
+namespace Agents.Enemies.Action
+{
+    [Serializable, GeneratePropertyBag]
+    [NodeDescription(name: "AnimationEnd", story: "[Enemy] animation end", category: "Action", id: "c2e12e3d079efe9d5e786b131756ead7")]
+    public partial class AnimationEndAction : Unity.Behavior.Action
+    {
+        [SerializeReference] public BlackboardVariable<AbstractEnemy> Enemy;
+
+        private AgentTriggerModule _triggerModule;
+        
+        private bool _animationEnd = false;
+        protected override Status OnStart()
+        {
+            if(Enemy == null || Enemy.Value == null)
+                return Status.Failure;
+
+            _triggerModule = Enemy.Value.Trigger;
+            
+            if(_triggerModule == null)
+                return Status.Failure;
+
+            _triggerModule.OnAnimationEnd += HandleAnimationEnd;
+
+            return Status.Running;
+        }
+
+        private void HandleAnimationEnd()
+        {
+            _triggerModule.OnAnimationEnd -= HandleAnimationEnd;
+            _animationEnd = true;
+        }
+
+        protected override Status OnUpdate()
+        {
+            return _animationEnd ? Status.Success : Status.Running;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Agents/Enemies/CamoDeserts/CamoDesert.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Agents.Enemies.CamoDeserts
+{
+    public class CamoDesert : AbstractEnemy
+    {
+        
+    }
+}
