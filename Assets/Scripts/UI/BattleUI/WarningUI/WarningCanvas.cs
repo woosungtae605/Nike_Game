d47604e6cd102bc1dbc5747b2166a449413b810e
woosungtae <a01076755795@gmail.com>
2026-06-14 17:12:18 +0900

    feat : WaringUI

---
+++ Assets/Scripts/UI/BattleUI/WarningUI/WarningCanvas.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UI.BattleUI.WarningUI
+{
+    public class WarningCanvas : MonoBehaviour, IUIElement
+    {
+        [SerializeField] private GameObject warning;
+        public void Hide()
+        {
+            
+        }
+    }
+}
