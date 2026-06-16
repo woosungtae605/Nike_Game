ecb0a529949aba17ba36558f45d9c5cca72eb54d
woosungtae <a01076755795@gmail.com>
2026-06-16 17:02:45 +0900

    save

--- Assets/Scripts/UI/MainSceneUI/Nikkes/NikkeProfiles/NikkeProfile.cs
+++ Assets/Scripts/UI/MainSceneUI/Nikkes/NikkeProfiles/NikkeProfile.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using Agents.Players;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace UI.MainSceneUI.Nikkes.NikkeProfiles
 {
-    public class NikkeProfile : MonoBehaviour
+    public class NikkeProfile : MonoBehaviour, IUIElement<PlayerDataSO>
     {
         [SerializeField] private TextMeshProUGUI levelText;
         [SerializeField] private TextMeshProUGUI nameText;
         [SerializeField] private Image profileImage;
         [SerializeField] private Button clickBtn;
+        
+        public void Show(PlayerDataSO item)
+        {
+            
+        }
+
+        public void Hide()
+        {
+            
+        }
     }
 }
