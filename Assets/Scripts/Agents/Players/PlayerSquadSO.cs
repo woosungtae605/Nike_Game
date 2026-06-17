6ba94df82ba2bf0263db2ff3ab6541fdb5701a99
woosungtae <a01076755795@gmail.com>
2026-06-17 17:42:43 +0900

    save

--- Assets/Scripts/Agents/Players/PlayerSquadSO.cs
+++ Assets/Scripts/Agents/Players/PlayerSquadSO.cs
@@ -6,6 +6,6 @@ namespace Agents.Players
     [CreateAssetMenu(fileName = "PlayerSquad", menuName = "SO/PlayerSquad", order = 0)]
     public class PlayerSquadSO : ScriptableObject
     {
-        List<PlayerDataSO>  playerDataSos = new List<PlayerDataSO>();
+        public PlayerDataSO[] PlayerDataSos { get; private set; } = new PlayerDataSO[5];
     }
 }
---
+++ Assets/Scripts/UI/MainSceneUI/Squards/SquadPlusUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Agents.Players;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.MainSceneUI.Squards
+{
+    public class SquadPlusUI : MonoBehaviour
+    {
+        [SerializeField] private PlayerSquadSO playerSquad;
+        [SerializeField] private Image[] images = new Image[5];
+        [SerializeField] private Sprite noEquip;
+        public void Init()
+        {
+            for (int i = 0; i < playerSquad.PlayerDataSos.Length; i++)
+            {
+                images[i].sprite =  playerSquad.PlayerDataSos[i] != null ? playerSquad.PlayerDataSos[i].NikkeSprite : noEquip;
+            }
+        }
+    }
+}
