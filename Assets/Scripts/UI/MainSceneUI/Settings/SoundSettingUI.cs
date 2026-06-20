14c8f226ef55e4a449319d68e40f5a6b27cda710
woosungtae <a01076755795@gmail.com>
2026-06-20 22:18:06 +0900

    feat : settingCanvas frame

--- Assets/Scripts/UI/MainSceneUI/Settings/SoundSettingUI.cs
+++ Assets/Scripts/UI/MainSceneUI/Settings/SoundSettingUI.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.MainSceneUI.Settings
 {
     public class SoundSettingUI : MonoBehaviour
     {
-        
+        [Header("Sound Settings")]
+        [SerializeField] private Button exitBtn;
+        [SerializeField] private Slider masterSlider;
+        [SerializeField] private Toggle masterToggle;
+        [SerializeField] private Slider bgmSlider;
+        [SerializeField] private Toggle bgmToggle;
+        [SerializeField] private Slider sfxSlider;
+        [SerializeField] private Toggle sfxToggle;
     }
 }
