306d0f561a2b7ec522593deb4e6bbdef345740e5
woosungtae <a01076755795@gmail.com>
2026-06-21 19:54:18 +0900

    feat : Quit

---
+++ Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class QuitButton : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
