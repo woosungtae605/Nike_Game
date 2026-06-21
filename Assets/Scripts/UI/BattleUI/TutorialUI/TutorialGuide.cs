84a4303195d90fa9f3ca7408aaa099695f3c7c6e
woosungtae <a01076755795@gmail.com>
2026-06-21 13:16:27 +0900

    save

---
+++ Assets/Scripts/UI/BattleUI/TutorialUI/TutorialGuide.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.BattleUI.TutorialUI
+{
+    public class TutorialGuide : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI tutorialText;
+        [SerializeField] protected Image characterImage;
+        public void TutorialGo(string text, Sprite sprite)
+        {
+            characterImage.sprite = sprite;
+            tutorialText.text = text;
+            Time.timeScale = 0;
+        }
+    }
+}
